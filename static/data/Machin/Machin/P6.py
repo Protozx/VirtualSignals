20de6abe59d5e4e4812852ec59d4dd8b58bd1eb5
Protozx <83438783+Protozx@users.noreply.github.com>
2023-12-06 12:03:30 -0600

    mejor
    va mejor

--- static/data/Machin/Machin/P6.py
+++ static/data/Machin/Machin/P6.py
@@ -18,57 +18,13 @@ def cargar_datos(ruta_csv):
     y = data.iloc[:, -1].values
     return X, y
 
-def k_fold_cross_validation(X, y, k, modelo):
-    fold_size = len(X) // k
-    indices = np.arange(len(X))
-    np.random.shuffle(indices)
-    scores = []
-
-    for fold in range(k):
-        test_indices = indices[fold * fold_size : (fold + 1) * fold_size]
-        train_indices = np.setdiff1d(indices, test_indices)
-
-
-        X_train, X_test = X[train_indices], X[test_indices]
-        y_train, y_test = y[train_indices], y[test_indices]
-
-
-        modelo_clonado = clone(modelo)
-        modelo_clonado.fit(X_train, y_train)
-        
-        
-        y_pred = modelo_clonado.predict(X_test)
-        score = accuracy_score(y_test, y_pred)
-        scores.append(score)
-
-    return scores
-
-def evaluar_con_kfold_manual(X, y, k=5):
-    modelos = {
-        'KNN': Pipeline([('scaler', StandardScaler()), ('classifier', KNeighborsClassifier(n_neighbors=5))]),
-        'Regresión Logística': Pipeline([('scaler', StandardScaler()), ('classifier', LogisticRegression(random_state=42))]),
-        'Maquina de soporte vectorial': Pipeline([('scaler', StandardScaler()), ('classifier', SVC(kernel='linear', random_state=42))]),
-        'Clasificador Bayesiano': Pipeline([('scaler', StandardScaler()), ('classifier', GaussianNB())])
-    }
-
-    resultados = dict()
-    resultados['Cadenas ocultas de markov'] =  puntaje_markov('dataset.csv')
-    print(f"\n\nCadenas ocultas de markov: {resultados['Cadenas ocultas de markov']}")
-    for nombre, modelo in modelos.items():
-        scores = k_fold_cross_validation(X, y, k, modelo)
-        resultados[nombre] = scores
-        print(f"{nombre}: {np.mean(scores)}")
-    mejor_modelo = max(resultados, key=lambda nombre: np.mean(resultados[nombre]))
-    
-    print(f"\nEl mejor modelo es {mejor_modelo} con una precisión de {np.mean(resultados[mejor_modelo])}.")
-
-
 def cargar_datos_markov(filename):
     columns = ['Time', 'X', 'Y', 'Z', 'R', 'Theta', 'label']
     data = pd.read_csv(filename, header=None, names=columns)
     label_encoder = LabelEncoder()
     data['label'] = label_encoder.fit_transform(data['label'])
     return data, label_encoder
+
 
 def separar_datos_markov(data):
     X = data[['Time', 'X', 'Y', 'Z', 'R', 'Theta']]
@@ -120,6 +76,54 @@ def puntaje_markov(archivo):
     data, label_encoder = cargar_datos_markov(archivo)
     accuracy_promedio = validacion_cruzada_kfold(data, 5)
     return accuracy_promedio
+
+def k_fold_cross_validation(X, y, k, modelo):
+    fold_size = len(X) // k
+    indices = np.arange(len(X))
+    np.random.shuffle(indices)
+    scores = []
+
+    for fold in range(k):
+        test_indices = indices[fold * fold_size : (fold + 1) * fold_size]
+        train_indices = np.setdiff1d(indices, test_indices)
+
+
+        X_train, X_test = X[train_indices], X[test_indices]
+        y_train, y_test = y[train_indices], y[test_indices]
+
+
+        modelo_clonado = clone(modelo)
+        modelo_clonado.fit(X_train, y_train)
+        
+        
+        y_pred = modelo_clonado.predict(X_test)
+        score = accuracy_score(y_test, y_pred)
+        scores.append(score)
+
+    return scores
+
+def evaluar_con_kfold_manual(X, y, k=5):
+    modelos = {
+        'KNN': Pipeline([('scaler', StandardScaler()), ('classifier', KNeighborsClassifier(n_neighbors=5))]),
+        'Regresión Logística': Pipeline([('scaler', StandardScaler()), ('classifier', LogisticRegression(random_state=42))]),
+        'Maquina de soporte vectorial': Pipeline([('scaler', StandardScaler()), ('classifier', SVC(kernel='linear', random_state=42))]),
+        'Clasificador Bayesiano': Pipeline([('scaler', StandardScaler()), ('classifier', GaussianNB())])
+    }
+
+    resultados = dict()
+    resultados['Cadenas ocultas de markov'] =  puntaje_markov('dataset.csv')
+    print(f"\n\nCadenas ocultas de markov: {resultados['Cadenas ocultas de markov']}")
+    
+    for nombre, modelo in modelos.items():
+        scores = k_fold_cross_validation(X, y, k, modelo)
+        resultados[nombre] = scores
+        print(f"{nombre}: {np.mean(scores)}")
+    mejor_modelo = max(resultados, key=lambda nombre: np.mean(resultados[nombre]))
+    
+    print(f"\nEl mejor modelo es {mejor_modelo} con una precisión de {np.mean(resultados[mejor_modelo])}.")
+
+
+
 
 def main(ruta_csv):
     X, y = cargar_datos(ruta_csv)
